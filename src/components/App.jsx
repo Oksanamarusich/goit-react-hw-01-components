3185e129c9c0c5e86927e04a0da884a77cf1dddc
Oksanamarusich <oksana.m229@gmail.com>
2023-09-29 20:45:23 +0300

    add

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -9,7 +9,7 @@ import data from '../data/data.json';
 
 import { FriendList } from './FriendList/FriendList'
 import friends from '../data/friends.json';
-import { FriendListItem } from './FriendList/FriendListItem';
+
 
 
 import { TransactionHistory } from './Transaction/TransactionHistory';
