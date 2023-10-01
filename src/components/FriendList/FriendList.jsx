4e238d0a16621f594793a6719cf1fcb996926c67
Oksanamarusich <oksana.m229@gmail.com>
2023-10-01 14:10:49 +0300

    add folder

--- src/components/FriendList/FriendList.jsx
+++ src/components/FriendList/FriendList.jsx
@@ -1,5 +1,5 @@
 import PropTypes from "prop-types";
-import { FriendListItem } from "./FriendListItem";
+import { FriendListItem } from "../FriendListItem/FriendListItem";
 import {UlFriendList} from "../FriendList/FriendList.styled"
 
 export const FriendList = ({ friends }) => {
